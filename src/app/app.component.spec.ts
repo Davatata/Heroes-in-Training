712c62544bf0cda62b5a7262511e13961fe1c669
David <Davatata@users.noreply.github.com>
2018-09-26 17:49:01 -0700

    Initial Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Heroes-in-Training'`, async(() => {
+  it(`should have as title 'Heroes in Training'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Heroes-in-Training');
+    expect(app.title).toEqual('Heroes in Training');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Heroes-in-Training!');
+    expect(compiled.querySelector('h1').textContent).toContain('Heroes in Training');
   }));
 });
